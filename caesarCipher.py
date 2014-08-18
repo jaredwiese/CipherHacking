73ef4341c38354c2818598f742084daa6413980b
wiesejc <wiesejc@users.noreply.github.com>
2014-08-18 04:47:44 -0500

    Create caesarCipher.py

---
+++ caesarCipher.py
@@ -0,0 +1,56 @@
+# Caesar Cipher
+# http://inventwithpython.com/hacking (BSD Licensed)
+# * forked by Jared Wiese
+
+import pyperclip
+
+# the string to be encrypted/decrypted
+# message = 'Men willingly believe what they wish.' # test message
+message = raw_input('Enter Text ').upper()
+
+# the encryption/decryption key
+key = int(raw_input('Key '))
+
+# tells the program to encrypt or decrypt
+while True:
+    mode = raw_input('Encrypt or Decrypt? ').lower()
+    if mode.strip() == 'encrypt' or mode.strip() == 'decrypt':
+        break
+
+# every possible symbol that can be encrypted
+LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+# stores the encrypted/decrypted form of the message
+translated = ''
+
+# run the encryption/decryption code on each symbol in the message string
+for symbol in message:
+    if symbol in LETTERS:
+        # get the encrypted/decrypted number for this symbol
+        num = LETTERS.find(symbol) # get the number of the symbol
+        if mode == 'encrypt':
+            num = num + key
+        elif mode == 'decrypt':
+            num = num - key
+            
+        # handle the wrap-around if num is larger than the length of 
+        # LETTERS or less than 0
+        if num >= len(LETTERS):
+            num = num -len(LETTERS)
+        elif num < 0:
+            num = num + len(LETTERS)
+        
+        # add encrypted/decrypted numbers's symbol at the end of translated
+        translated += LETTERS[num]
+
+    else:
+        # just add the symbol without encrypting/decrypting
+        translated += symbol
+        
+# print the encrypted/decrypted string to the screen
+print translated
+
+# copy the encrypted/decrypted string to the clipboard
+pyperclip.copy(translated)
+
+exit()
