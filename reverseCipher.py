b39cd5aef3722fecdcbc456942081af56351ff3d
Jared <wiesejc@users.noreply.github.com>
2014-08-20 02:44:18 -0500

    Update reverseCipher.py

--- reverseCipher.py
+++ reverseCipher.py
@@ -1,6 +1,6 @@
 # The reverse cipher encrypts a message by printing it in reverse order.
 # http://inventwithpython.com/hacking
-# * forked by Jared Wiese
+# * Forked by Jared Wiese
 
 # plainText = 'Three can keep a secret, if two of them are dead.' # Test message
 plainText = raw_input('Enter Message: ')
